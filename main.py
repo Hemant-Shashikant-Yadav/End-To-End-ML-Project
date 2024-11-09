7765cc5128effba3f5be8a28b5257653db055e35
Hemant Shashikant Yadav <hemantsryadav@gmail.com>
2024-11-09 23:25:48 +0530

    Stage 2 data validation is completed and added

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from src.mlProject.logging import logger
 from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
@@ -12,3 +13,14 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+STAGE_NAME = "Data Validation stage"
+
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataValidationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
